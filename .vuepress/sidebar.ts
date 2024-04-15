693c27c7b9cb1215b6d698fe040bb3d51106689d
wml107 <weimulun@126.com>
2024-04-15 09:44:31 +0800

    update: 写了一些新东西

--- .vuepress/sidebar.ts
+++ .vuepress/sidebar.ts
@@ -1,10 +1,7 @@
 import {SidebarConfig4Multiple} from "vuepress/config";
 
-import compositionPrinciple from "./sidebars/fundmental/compositionPrinciple";
 // @ts-ignore
 export default {
-    //基本功
-    "/docs/基本功/计算机组成原理/": compositionPrinciple,
     // 降级，默认根据文章标题渲染侧边栏
     "/": "auto",
 } as SidebarConfig4Multiple;
